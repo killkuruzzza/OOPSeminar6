56b672a9b91a7282407399200e14fc35bb84d038
Alexey Golikov <lexgolikoff@gmail.com>
2023-10-15 20:33:09 +0300

    add comments to LSP

--- src/LSP/Test.java
+++ src/LSP/Test.java
@@ -1,6 +1,10 @@
 package LSP;
 
 public class Test {
+//    Проверяем класс QuadRangle и отнаследованные от него классы Rectangle и Square,  если мы заменим все использования класса
+//    QuadRangle на Rectangle или Square, ничего не меняется в работе программы. Ведь классы Rectangle и Square всего лишь
+//    расширяют функционал класса QuadRangle.
+//    Проверка работает, значит программа соответствует принципу подстановки Лисков.
     public static void main(String[] args) {
         QuadRangle quadRangle = new Rectangle(5, 3);
         System.out.println("Rectangle" + " " + quadRangle.area());
