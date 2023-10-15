6de82e916a448cf75753cb77cd4af3b2f835fddd
Alexey Golikov <lexgolikoff@gmail.com>
2023-10-15 21:28:04 +0300

    add comments to ISP

--- src/ISP/Shape3D.java
+++ src/ISP/Shape3D.java
@@ -1,5 +1,6 @@
 package ISP;
 
-public interface Shape3D extends Shape{
+public interface Shape3D {
+    // В интерфейсе один метод, т.к. он может быть применен только к 3D фигурам.
     public double volume();
 }
